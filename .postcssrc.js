a27c9ce0b0923997f1e35be03d4e77fc2c3b2a3c
zhufan <125920969@qq.com>
2017-07-06 15:29:25 +0800

    使用postcss-px2rem

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,8 +1,11 @@
 // https://github.com/michael-ciniawsky/postcss-load-config
 
 module.exports = {
-  "plugins": {
+  plugins: {
     // to edit target browsers: use "browserlist" field in package.json
-    "autoprefixer": {}
-  }
-}
+    autoprefixer: {
+      browsers: ['iOS >= 7', 'Android >= 2.3', 'IE >= 7'],
+    },
+    'postcss-px2rem': { remUnit: 64 },
+  },
+};

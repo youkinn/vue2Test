0f471c1eb94c28bda2691ce9b1a92c6956e9b9fe
zhufan <125920969@qq.com>
2017-07-06 17:05:48 +0800

    调整px2rem的比例

--- .postcssrc.js
+++ .postcssrc.js
@@ -3,9 +3,9 @@
 module.exports = {
   plugins: {
     // to edit target browsers: use "browserlist" field in package.json
+    'postcss-px2rem': { remUnit: 100 },
     autoprefixer: {
       browsers: ['iOS >= 7', 'Android >= 2.3', 'IE >= 7'],
     },
-    'postcss-px2rem': { remUnit: 64 },
   },
 };

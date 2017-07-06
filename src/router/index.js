fbbaea2416b02ab973df94410afa4959ba2fb831
zhufan <125920969@qq.com>
2017-07-06 15:27:25 +0800

    添加home路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,43 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Hello from '@/components/Hello';
+// import Hello from '@/components/Hello';
 
 Vue.use(Router);
 
+/* eslint-disable global-require*/
 export default new Router({
+  mode: 'history', // history模式
+  // 参考 https://segmentfault.com/q/1010000008343428
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition || typeof savedPosition === 'undefined') {
+      // 从第二页返回首页时savePosition为undefined
+      // 只处理设置了路由元信息的组件
+      /* eslint-disable no-param-reassign*/
+      from.meta.isKeepAlive = typeof from.meta.isKeepAlive === 'undefined' ? undefined : false;
+      to.meta.isKeepAlive = typeof to.meta.isKeepAlive === 'undefined' ? undefined : true;
+      if (savedPosition) {
+        return savedPosition;
+      }
+    } else {
+      from.meta.isKeepAlive = typeof from.meta.isKeepAlive === 'undefined' ? undefined : true;
+      to.meta.isKeepAlive = typeof to.meta.isKeepAlive === 'undefined' ? undefined : false;
+    }
+    return null;
+  },
   routes: [
     {
+      name: 'home', // 首页
       path: '/',
-      name: 'Hello',
-      component: Hello,
+      component(resolve) {
+        require.ensure(
+          [],
+          () => {
+            resolve(require('../pages/home/home/homePage.vue'));
+          },
+          'home',
+        );
+      },
+      meta: { isKeepAlive: true },
     },
   ],
 });

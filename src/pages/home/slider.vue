daa071a39ffe9aaa921934bd9081b3c84da1470c
zhufan <125920969@qq.com>
2017-07-07 14:49:10 +0800

    边框

---
+++ src/pages/home/slider.vue
@@ -0,0 +1,13 @@
+<!--轮播图-->
+<template>
+  <div class="swiper-container">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide" v-for="(item, index) in arrSwiperList" :key="index">
+        <img :src="item.imgUrl" alt="">
+      </div>
+    </div>
+    <div class="swiper-pagination"></div>
+  </div>
+</template>
+<script src="./slider.js"></script>
+<style lang="scss" scoped src="./slider.scss"></style>

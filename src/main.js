7fb79ad23a8bfa7adcb865eba40bbaea3cf400b7
zhufan <125920969@qq.com>
2017-07-07 13:34:13 +0800

    添加es6-promise,兼容老浏览器与ie11

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 
+require('es6-promise/auto');
+
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */

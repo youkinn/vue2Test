41a9becb3a64e1154a279083eb301fb0ae754a87
zhufan <125920969@qq.com>
2017-07-10 15:28:46 +0800

    合并成单文件

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,26 @@
   </div>
 </template>
 
-<script src="./App.js"></script>
-<style lang="scss" src="./App.scss"></style>
+<script>
+import '../static/css/common.css';
+import '../static/icons/style.css';
+
+import flexbox from './util/flexbox';
+
+// 定义页面rem的font-size
+flexbox();
+window.onresize = flexbox;
+
+export default {
+  name: 'app',
+};
+</script>
+<style lang="scss">
+#app {
+  min-height: 100vh;
+  max-width: 640px;
+  min-width: 640px;
+  margin: auto;
+}
+</style>
 

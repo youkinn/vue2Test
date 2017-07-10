6e200461e6799377c9f47b3bbce9ef0900df7ef0
zhufan <125920969@qq.com>
2017-07-10 14:45:18 +0800

    合并成为单页面文件

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -16,14 +16,131 @@
       <hot-game-component></hot-game-component>
     </div>
     <!--<div class="hotNews section">
-                  <div class="hotNewsContainer">
-                    <hot-news-component :o-news="news"></hot-news-component>
-                  </div>
-                </div>-->
+          <div class="hotNewsContainer">
+            <hot-news-component :o-news="news"></hot-news-component>
+          </div>
+        </div>-->
     <back-to-top-component></back-to-top-component>
     <app-footer-component></app-footer-component>
   </div>
   <fading-circle-compontent v-else></fading-circle-compontent>
 </template>
-<script src="./home.js"></script>
-<style lang="scss" scoped src="./home.scss"></style>
+<script>
+import { Swipe, SwipeItem } from 'mint-ui';
+import Vue from 'vue';
+import appHeaderComponent from '../../components/header/header.vue';
+import fadingCircleCompontent from '../../components/fadingCircle/fadingCircle.vue';
+import appFooterComponent from '../../components/footer/footer.vue';
+import backToTopComponent from '../../components/toTopBtn/toTopBtn.vue';
+import headerComponent from './header.vue';
+import sliderComponent from './slider.vue';
+import hotGameComponent from './hotGame.vue';
+import { getSwiperList } from '../../http/home';
+
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
+
+export default {
+  components: {
+    appHeaderComponent, // 头部通用
+    fadingCircleCompontent, // 圆形载入动画
+    appFooterComponent, // 底部导航条
+    backToTopComponent, // 返回顶部的按钮
+    headerComponent, // 小模块标题
+    sliderComponent, // 轮播模块
+    hotGameComponent, // 热门游戏
+  },
+  data() {
+    return {
+      title: '首页',
+      swiperList: [],
+      news: {},
+      activated: false,
+      inited: true,
+    };
+  },
+  methods: {
+    fetchData() {
+      const promises = [this.getSwiperList, this.getNewsList].map(fn => fn());
+      Promise.all(promises).then(() => {
+        this.inited = true;
+      });
+    },
+    getSwiperList() {
+      getSwiperList(1)
+        .then((res) => {
+          const data = res.data;
+          if (Number(data.code) === 10000) {
+            this.swiperList = res.data.result;
+          }
+        })
+        .catch(error => error);
+      // this.swiper = new LoadData(Vue.ClientUrl.getSwiperList, {
+      //   type: 4,
+      //   limit: 8,
+      //   nologin: 1,
+      // });
+      // return this.swiper.getList(this, () => {
+      //   setTimeout(() => {
+      //     new Swiper('.swiper-container', {
+      //       direction: 'horizontal',
+      //       loop: true,
+      //       pagination: '.swiper-pagination',
+      //       autoplay: 3500,
+      //     });
+      //   }, 100);
+      // });
+    },
+    getNewsList() {
+      // this.news = new LoadData(Vue.ClientUrl.getNewsList, {
+      //   limit: 200,
+      //   nologin: 1,
+      // });
+      // return this.news.getList();
+    },
+  },
+  created() {
+    this.fetchData();
+  },
+};
+</script>
+<style lang="scss" scoped>
+.action-button {
+  background-color: yellow;
+}
+
+.container {
+  .section {
+    background-color: #fff;
+    &:not(:first-child) {
+      border-top: solid #e5e5e5 1px;
+    }
+    &:not(:last-child) {
+      margin-bottom: 30px;
+      border-bottom: solid #e5e5e5 1px;
+    }
+  }
+  .swiper-contanier {
+    height: 280px;
+    overflow: hidden;
+    padding-left: 30px;
+    padding-right: 30px;
+  }
+  .hotGame.section {
+    overflow: hidden;
+    border-top: none;
+    .hr {
+      width: 610px;
+      height: 1px;
+      line-height: 1px;
+      border-top: solid #e5e5e5 1px;
+      margin-left: 30px;
+      color: #e5e5e5;
+    }
+  }
+  .section.hotNews {
+    margin-bottom: 99px;
+    border-bottom: none;
+  }
+}
+</style>
